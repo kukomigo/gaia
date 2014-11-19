5e90125aa622c311fa10d5ab63d06adbde9a25e3
Wojciech Lakomy <wojciech.lakomy@rec-global.com>
2014-11-19 09:40:59 +0100

    Version and buidId for About page

--- apps/privacy-panel/build/build.js
+++ apps/privacy-panel/build/build.js
@@ -3,7 +3,58 @@
 /* global require, exports, dump */
 var utils = require('utils');
 
+function hasGitCommand() {
+  return utils.getEnvPath().some(function(path) {
+    try {
+      var cmd = utils.getFile(path, 'git');
+      return cmd.exists();
+    } catch (e) {
+      // path not found
+    }
+    return false;
+  });
+}
+
 var PrivacyPanelAppBuilder = function() {};
+
+PrivacyPanelAppBuilder.prototype.readVersionFile = function(options) {
+  var aboutFile = utils.getFile(options.STAGE_APP_DIR, '/resources/about.json');
+  var aboutFileContent = utils.getFileContent(aboutFile);
+  return aboutFileContent;
+};
+
+PrivacyPanelAppBuilder.prototype.getLastCommit = function(options, callback) {
+  var gitDir = utils.getFile(options.GAIA_DIR, '.git');
+  if (gitDir.exists() && hasGitCommand()) {
+    var git = new utils.Commander('git');
+    var stderr, stdout;
+    var args = [
+      '--git-dir=' + gitDir.path,
+      'log',
+      '--format=%H',
+      'HEAD',
+      '-1'
+    ];
+
+    var cmdOptions = {
+      stdout: function(data) {
+        stdout = data;
+      },
+      stderr: function(data) {
+        stderr = data;
+      },
+      done: function(data) {
+        if (data.exitCode === 0) {
+          utils.log('privacy-panel-app-build', 'Last commit: ' + stdout);
+          callback(stdout);
+        }
+      }
+    };
+
+    git.initPath(utils.getEnvPath());
+    git.runWithSubprocess(args, cmdOptions);
+  }
+};
 
 PrivacyPanelAppBuilder.prototype.executeRjs = function(options) {
   var optimize = 'optimize=' +
@@ -21,6 +72,16 @@ PrivacyPanelAppBuilder.prototype.executeRjs = function(options) {
 
 PrivacyPanelAppBuilder.prototype.execute = function(options) {
   this.executeRjs(options);
+
+  this.getLastCommit(options, function(commit) {
+    var aboutFile = utils
+      .getFile(options.STAGE_APP_DIR, '/resources/about.json');
+    var aboutContent = utils
+      .readJSONFromPath(options.STAGE_APP_DIR + '/resources/about.json');
+
+    aboutContent.build = commit.substring(0, 10);
+    utils.writeContent(aboutFile, JSON.stringify(aboutContent));
+  });
 };
 
 exports.execute = function(options) {
